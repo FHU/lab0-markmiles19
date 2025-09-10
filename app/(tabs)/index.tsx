80208d4854b3bbb6486ccad3aa70bab23ae80789
Mark Miles <mark.miles@students.fhu.edu>
2025-09-10 13:30:14 -0500

    Backup
    The React logo has been swapped with a heart, and the background has been changed from blue to pink. Also, the waving emoji has been swapped with a couple holding hands.

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -9,15 +9,15 @@ import { ThemedView } from '@/components/ThemedView';
 export default function HomeScreen() {
   return (
     <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
+      headerBackgroundColor={{ light: '#ffc6fdff', dark: '#1D3D47' }}
       headerImage={
         <Image
-          source={require('@/assets/images/partial-react-logo.png')}
+          source={require('@/assets/images/partial-heart-logo.png')}
           style={styles.reactLogo}
         />
       }>
       <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
+        <ThemedText type="title">Perfect Date!</ThemedText>
         <HelloWave />
       </ThemedView>
       <ThemedView style={styles.stepContainer}>
@@ -69,7 +69,7 @@ const styles = StyleSheet.create({
     height: 178,
     width: 290,
     bottom: 0,
-    left: 0,
+    left: 50,
     position: 'absolute',
   },
 });
